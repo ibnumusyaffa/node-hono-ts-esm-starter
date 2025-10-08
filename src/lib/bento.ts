a9db3250766cc1478e789f619fbe1df5baafdbc2
Ibnu Musyaffa <ibnu.musyaffa@gmail.com>
2025-10-08 12:25:40 +0700

    update

--- src/lib/bento.ts
+++ src/lib/bento.ts
@@ -1,16 +1,22 @@
 import { BentoCache, bentostore } from "bentocache"
 import { redisDriver } from "bentocache/drivers/redis"
-import { Redis } from "ioredis";
-import env from "@/config/env.js";
+import { Redis } from "ioredis"
+import env from "@/config/env.js"
+import { logger } from "./logger.js"
 
+const client = new Redis(env.REDIS_URL)
 
-const client = new Redis(env.REDIS_URL);
+client.on("error", (err) => {
+  logger.error(err, err.message)
+})
 
 export const bento = new BentoCache({
-  default: 'redis',
+  default: "redis",
   stores: {
-    redis: bentostore().useL2Layer(redisDriver({
-      connection: client
-    }))
-  }
+    redis: bentostore().useL2Layer(
+      redisDriver({
+        connection: client,
+      })
+    ),
+  },
 })
